903374a802a32fb252404f83e0db43b18707a2d9
Hafsa Rashid <142868426+hafsaa05@users.noreply.github.com>
2025-03-01 08:53:06 +0500

    Update q1.py

--- Assignments/01/q1.py
+++ Assignments/01/q1.py
@@ -2,7 +2,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("heart_disease.csv")
+data = {
+    "ID": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    "Age": [28, 35, 42, 30, 50, 41, 29, 33, 45, 39],
+    "Gender": ["Male", "Female", "Male", "Female", "Male", "Female", "Male", "Female", "Male", "Female"],
+    "Department": ["IT", "HR", "Finance", "Marketing", "Sales", "HR", "IT", "Finance", "Sales", "Marketing"],
+    "Salary ($)": [70000, 60000, 90000, 65000, 85000, 62000, 75000, 88000, 87000, 67000],
+    "Experience (Years)": [3, 7, 12, 5, 15, 9, 4, 11, 14, 6],
+    "Work Hours/Week": [40, 38, 45, 42, 50, 37, 39, 44, 48, 41],
+    "Projects Completed": [5, 4, 6, 7, 8, 5, 6, 7, 9, 6],
+    "Job Satisfaction (1-10)": [7, 8, 6, 9, 5, 7, 8, 6, 5, 8],
+    "Training Hours": [20, 15, 18, 25, 12, 16, 22, 20, 14, 19],
+    "Promotion Status": ["No", "Yes", "Yes", "No", "Yes", "No", "No", "Yes", "Yes", "No"]
+}
+
+df = pd.DataFrame(data)
 
 print(f"Dataset contains {df.shape[0]} rows and {df.shape[1]} columns.\n")
 print("Summary statistics:\n", df.describe())
